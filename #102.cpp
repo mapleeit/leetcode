a1a0e87bdaa247e9b6bb9644eaff409f52b35261
Eric Miao <522856232@qq.com>
2015-01-06 19:38:27 +0800

    #102

---
+++ #102.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> result;
+    void buildVector(TreeNode *root, int depth){
+        if(root == NULL) return;
+        if(result.size() == depth)
+            result.push_back(vector<int>());
+        result[depth].push_back(root->val);
+        buildVector(root->left, depth + 1);
+        buildVector(root->right, depth + 1);
+    }
+    vector<vector<int> > levelOrder(TreeNode *root) {
+        buildVector(root, 0);
+        return result;
+    }
+};
