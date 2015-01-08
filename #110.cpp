8dd773c6e5ad22ee797ba00549bf76771d4ff85f
Eric Miao <522856232@qq.com>
2015-01-08 13:26:05 +0800

    #110 version2

--- #110.cpp
+++ #110.cpp
@@ -38,3 +38,39 @@ public:
     	return tempLeft > tempRight ? tempLeft + 1 : tempRight + 1;
     }
 };
+
+// version 2 : Accepted
+// more efficient
+
+class Solution {
+public:
+
+    bool isBalanced(TreeNode *root) {
+    	if (root == NULL)
+    	{
+    	    return true;
+    	}    
+    	return isBalancedAux(root) == UNBALANCED ? false : true;
+    }
+
+    static const int UNBALANCED = -1;
+
+    int isBalancedAux(TreeNode *root){
+    	if (root == NULL)
+    	{
+    		return 0;
+    	}
+    	int tempLeft = isBalancedAux(root->left),
+    		tempRight = isBalancedAux(root->right);
+    	if (tempLeft == UNBALANCED || tempRight == UNBALANCED){
+    	    return UNBALANCED;
+    	}
+    	if (abs(tempLeft - tempRight) > 1)
+    	{
+    		return UNBALANCED;
+    	}else{
+    		return tempLeft > tempRight ? tempLeft + 1 : tempRight + 1;
+    	}
+
+    }
+};
